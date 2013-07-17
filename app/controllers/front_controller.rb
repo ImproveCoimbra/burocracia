0e83d2ab9c57ed1e1f55cc1f09609e4865a2e871
Sergio Santos <me@sergiosantos.info>
2013-07-17 12:14:49 +0100

    Charts are back

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -4,7 +4,7 @@ class FrontController < ApplicationController
     if params[:q].present?
       search = Document.limit(1000).text_search('"'+params[:q]+'"')
       @stats = search.stats
-      @documents = search.entries.sort_by! { |doc| doc.date }
+      @documents = search.entries.sort_by!{ |doc| doc.date }.reverse
     else
       @documents = Document.all.only(:title, :date).order_by(:date => :desc)
     end
