d17ca6c547127312762735cc46c36b9dac3b22c8
Sérgio Santos <me@sergiosantos.info>
2014-12-31 19:22:51 +0000

    Use new text search method to remove bug of too many search results

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -2,9 +2,8 @@ class FrontController < ApplicationController
 
   def index
     if params[:q].present?
-      search = Document.limit(1000).text_search('"'+params[:q]+'"').project('_id' => 1, :date => 1)
-      @stats = search.stats
-      @all_documents = search.entries.sort_by!{ |doc| doc.date }.reverse
+      search = Document.collection.find( {'$text' => { '$search' => '"'+params[:q]+'"' }}).select( { 'date' => 1} )
+      @all_documents = search.entries.map{|d| Document.new(d) }.sort_by{ |doc| doc.date }.reverse
       @documents = Document.where(:id.in => @all_documents.map(&:id)).order_by(:date => :desc).page(params[:page]).per(5)
     else
       @documents = Document.all.only(:id, :title, :date).order_by(:date => :desc)
