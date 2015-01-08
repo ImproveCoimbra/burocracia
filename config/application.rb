2be83a53b840042d5556b4f9ab3e66fe57389ae8
Sérgio Santos <me@sergiosantos.info>
2015-01-08 23:11:41 +0000

    Compile assets

--- config/application.rb
+++ config/application.rb
@@ -48,9 +48,5 @@ module Docs
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    # Heroku stuff
-    config.assets.initialize_on_precompile = false
-
   end
 end
